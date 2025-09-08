75231ccfdb8f7b6a64f38208b16421eb15f8f7f8
Vis-halV <vishal06076@gmail.com>
2025-09-08 23:22:02 +0530

    chore: setup Tailwind CSS

--- auth-app/src/App.jsx
+++ auth-app/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1> Template cleared. </h1> 
+      <h1 className='text-black bg-green-200'> Template cleared. </h1> 
     </>
   )
 }
