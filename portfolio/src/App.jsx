24dc8f3ae9e0ecee43c7e53071e92d180de74996
Vishal V <168957641+Vis-halV@users.noreply.github.com>
2025-09-10 16:13:38 +0000

    feat: add DepartureMono and JetBrains Mono fonts
    feat: added departure mono and jetbrains mono font
    
    feat: added departure mono and jetbrains mono font

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import './index.css';
 
+function App() {
   return (
     <>
+      <h1 className="font-departure text-2xl">
+        Departure Mono Example
+      </h1>
+
+      <p className="font-jetbrains text-lg">
+        JetBrains Mono Example
+      </p>
+
       <h1 className="text-3xl font-bold underline">Portfolio</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
